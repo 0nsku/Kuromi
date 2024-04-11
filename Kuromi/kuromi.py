2a742cdfc892e6401ae3da514e95e4b9a80b8e49
0nsku <143646543+0nsku@users.noreply.github.com>
2024-04-11 22:24:24 +0300

    Add files via upload

---
+++ Kuromi/kuromi.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Kuromi(commands.AutoShardedBot):
+    def __init__(self):
+        super().__init__(
+            help_command=SKID,
+            )
+        )
+        self.clown = you
+
+    async def on_ready(self):
+        print(f"skid")
+
+bot = Kuromi()
+
+bot.run('skid go away'), reconnect=True)
